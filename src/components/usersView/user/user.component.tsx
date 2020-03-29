8fec568d67e52c35a921de61562e98252b988523
daniel livshin <livshind12@gmail.com>
2020-03-29 23:17:13 +0300

    change letter in userAvatar

--- src/components/usersView/user/user.component.tsx
+++ src/components/usersView/user/user.component.tsx
@@ -15,7 +15,7 @@ export interface ColorProps {
 const User: FC<ColorProps> = ({ color }) =>
     <div className={"user-border"}>
         <div className={"user"} style={{ backgroundImage: `linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)),linear-gradient(${color.angle}, ${color.color1} ${color.percentColorOne}, ${color.color2} ${color.percentColorTwo})` }}>
-            <p className={"user-letter"} style={{ background: `linear-gradient(${color.angle}, ${color.color1} ${color.percentColorOne}, ${color.color2} ${color.percentColorTwo})` }}>D</p>
+            <p className={"user-letter"} style={{ background: `linear-gradient(${color.angle}, ${color.color1} ${color.percentColorOne}, ${color.color2} ${color.percentColorTwo})` }}>A</p>
         </div>
     </div>
 
